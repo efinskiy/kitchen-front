3a1de2a00f5fe4d2a664e9402b429ed533fab63d
Evgeniy <finskiy.ru@yandex.ru>
2022-06-02 01:31:29 +0300

    remove strict mode

--- src/index.js
+++ src/index.js
@@ -7,12 +7,12 @@ import { Helmet } from 'react-helmet';
 document.title = "Столовая";
 
 ReactDOM.render(
-  <React.StrictMode>
+  <>
     <Helmet>
       <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" />
     </Helmet>
     <App />
-  </React.StrictMode>,
+  </>,
   document.getElementById('root')
 );
 
